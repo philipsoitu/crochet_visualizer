eddc80777e7cc1c1db59efb9b069ef74529f60a2
philipsoitu <philip.soitu@gmail.com>
2026-01-18 10:53:49 -0500

    fix this

--- frontend/src/lib/components/header.svelte
+++ frontend/src/lib/components/header.svelte
@@ -35,7 +35,7 @@
           </a>
 
           <a
-            href="/new"
+            href="/projects/new"
             class="rounded-lg bg-primary px-4 py-2 text-sm font-medium text-primary-foreground hover:opacity-90"
           >
             + Add Project

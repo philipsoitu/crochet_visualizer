c55cfb3e37a34c7f6f78fb7f54756bf95816506b
philipsoitu <philip.soitu@gmail.com>
2026-01-18 10:14:36 -0500

    deploy test1

--- frontend/src/lib/components/header.svelte
+++ frontend/src/lib/components/header.svelte
@@ -35,7 +35,7 @@
           </a>
 
           <a
-            href="projects/new"
+            href="/new"
             class="rounded-lg bg-primary px-4 py-2 text-sm font-medium text-primary-foreground hover:opacity-90"
           >
             + Add Project
